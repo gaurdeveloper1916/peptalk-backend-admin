ee2cc5b10ac60dc05c77c41aee3f9e7b17baf340
kartik gaur <gaurdeveloper1916@gmail.com>
2025-04-29 17:02:39 +0530

    delete client side code

---
+++ peptalk-admin&backend/server/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+      {
+        name: 'peptalk_backend_dev',
+        script: 'npm',
+        args: 'run dev',
+        exp_backoff_restart_delay: 100,
+        env: {
+          NODE_ENV: "development",
+        },
+      },
+      {
+        name: 'peptalk_backend_prod',
+        script: 'npm',
+        args: 'dev',
+        exp_backoff_restart_delay: 100,
+        env_production: {
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
